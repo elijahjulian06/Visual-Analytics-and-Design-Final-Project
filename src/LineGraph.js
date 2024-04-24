b35c496aba1a1399396274fcf53d2913deb7e5ab
Elijah Julian <elijahjulian@Elijahs-MacBook-Pro.local>
2024-04-24 17:11:18 -0400

    made lines for line graph

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,37 +1,52 @@
-/** LineGraph.js
- * 
- * @description: This file graphs 
- * 
- * 
- * 
- */
+class LineGraph {
 
-class LineGraph{
-
-    constructor(con, data, root) {
-        
+    constructor(con, data, root, color) {
         this.con = con;
-        this.data = data;
-        this.root = root;
 
-        var margin = {top: 30, right: 50, bottom: 10, left: 50},
-            width = 460 - margin.left - margin.right,
-            height = 400 - margin.top - margin.bottom;
+        const margin = { top: 20, right: 20, bottom: 50, left: 50 };
+        const width = 800 - margin.left - margin.right;
+        const height = 800 - margin.top - margin.bottom;
+
+        const domain = ["2020", "2021", "2022", "2023"]; 
 
         const div = root.append('div')
             .style('width', '100%')
             .style('height', '100%');
 
-        this.svg = div.append('svg');
+        this.svg = div.append('svg')
+            .attr('width', width + margin.left + margin.right)
+            .attr('height', height + margin.top + margin.bottom)
+            .append('g')
 
-        const domain = ['2020', '2021', '2022', '2023'];
-        
+    
+            const x = d3.scalePoint()
+                .range([0, width])
+                .domain(domain);
+
+            const y = {};
+
+            domain.forEach(variable => {
+                y[variable] = d3.scaleLinear()
+                    .domain(d3.extent(data, d => +d[variable]))
+                    .range([height, 0]); 
+            });
+
+            const line = d3.line()
+                .x((d, i) => x(x.domain()[i])) 
+                .y((d, i) => y[x.domain()[i]](d));
+
   
-        //define x and y
-        //define line 
-        //define lines
-        //define domains for visual
-
+                  
+            const lines = this.svg.selectAll(".line")
+                .data(data)
+                .enter().append("path")
+                .attr("class", "line")
+                .attr("d", d => line(Object.values(d).slice(0, 4)))
+                .attr("fill", "none")
+                .style("stroke", d => color(d['job_title']))
+                .on("mouseover",)
+                .on("mouseout",)
     }
 
+    
 }
